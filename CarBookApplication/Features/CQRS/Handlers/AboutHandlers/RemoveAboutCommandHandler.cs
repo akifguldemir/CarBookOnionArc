e97e533be4a1516103950c0d85331f87dee19948
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-09 13:18:08 +0300

    commands and handlers created for about

---
+++ CarBookApplication/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBookApplication.Features.CQRS.Commands.AboutCommands;
+using CarBookApplication.Interfaces;
+using CarbookDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApplication.Features.CQRS.Handlers.AboutHandlers
+{
+    public class RemoveAboutCommandHandler
+    {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAboutCommand command)
+        {
+            var value = await _repository.GetByIdAsync(command.Id);
+            await _repository.RemoveAsync(value);
+        }
+    }
+}
