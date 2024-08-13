55eccca9c57645745c251d2916839f788241cd49
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-13 10:11:03 +0300

    Mediatr pattern created for FooterAddress

---
+++ CarBookApplication/Features/Mediator/Handlers/FooterAddressHandlers/GetFooterAddressByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using CarBookApplication.Features.Mediator.Queries.FooterAddressQueries;
+using CarBookApplication.Features.Mediator.Results.FeatureResults;
+using CarBookApplication.Features.Mediator.Results.FooterAddressResults;
+using CarBookApplication.Interfaces;
+using CarbookDomain.Entities;
+using MediatR;
+
+namespace CarBookApplication.Features.Mediator.Handlers.FooterAddressHandlers
+{
+    public class GetFooterAddressByIdQueryHandler : IRequestHandler<GetFooterAddressByIdQuery, GetFooterAddressByIdQueryResult>
+    {
+        private readonly IRepository<FooterAddress> _repository;
+
+        public async Task<GetFooterAddressByIdQueryResult> Handle(GetFooterAddressByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetFooterAddressByIdQueryResult
+            {
+                Address = values.Address,
+                Description = values.Description,
+                Email = values.Email,
+                Phone = values.Phone,
+                FooterAddressID = values.FooterAddressID    
+            };
+        }
+    }
+}
---
+++ CarBookApplication/Features/Mediator/Queries/FooterAddressQueries/GetFooterAddressQuery.cs
@@ -0,0 +1,9 @@
+﻿using CarBookApplication.Features.Mediator.Results.FooterAddressResults;
+using MediatR;
+
+namespace CarBookApplication.Features.Mediator.Queries.FooterAddressQueries
+{
+    public class GetFooterAddressQuery : IRequest<List<GetFooterAddressQueryResult>>
+    {
+    }
+}
