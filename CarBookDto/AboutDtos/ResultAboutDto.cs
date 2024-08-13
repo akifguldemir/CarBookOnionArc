f07d31cf59709062ed757a4dac043e2eadcab335
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-13 16:03:21 +0300

    partial views creted

---
+++ CarBookDto/AboutDtos/ResultAboutDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBookDto.AboutDtos
+{
+	public class ResultAboutDto
+    {
+        public int aboutID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imageUrl { get; set; }
+
+    }
+}
