05983b4b182c26e9f9230b3e205c78b3e401ec8d
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-22 10:17:14 +0300

    ICarPricingRepository created

---
+++ CarBookApplication/Interfaces/CarPricingInterfaces/ICarPricingRepository.cs
@@ -0,0 +1,15 @@
+﻿using CarbookDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBookApplication.Interfaces.CarPricingInterfaces
+{
+    public interface ICarPricingRepository
+    {
+        List<CarPricing> GetCarPricingWithCars();
+
+    }
+}
---
+++ CarBookPersistence/Repositories/CarPricingRepositories/CarPricingRepository.cs
@@ -0,0 +1,23 @@
+﻿using CarBookApplication.Interfaces.CarPricingInterfaces;
+using CarbookDomain.Entities;
+using CarBookPersistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarBookPersistence.Repositories.CarPricingRepositories
+{
+    public class CarPricingRepository : ICarPricingRepository
+    {
+        private readonly CarBookContext _context;
+
+        public CarPricingRepository(CarBookContext context)
+        {
+            _context = context;
+        }
+
+        public List<CarPricing> GetCarPricingWithCars()
+        {
+            var values = _context.CarPricings.Include(x => x.Car).ThenInclude(y => y.Brand).Include(x => x.Pricing).ToList();
+            return values;
+        }
+    }
+}
