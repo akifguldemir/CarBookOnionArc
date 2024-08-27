4b872830c2853d683a06c3ff5232f8ead202c63e
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-27 15:59:21 +0300

    Car Count mediator complete

---
+++ CarBookApplication/Features/Mediator/Handlers/StatisticsHandlers/GetCarCountQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using CarBookApplication.Features.Mediator.Queries.StatisticsQueries;
+using CarBookApplication.Features.Mediator.Results.StatisticsResults;
+using CarBookApplication.Interfaces.CarInterfaces;
+using MediatR;
+
+namespace CarBookApplication.Features.Mediator.Handlers.StatisticsHandlers
+{
+    public class GetCarCountQueryHandler : IRequestHandler<GetCarCountQuery, GetCarCountQueryResult>
+    {
+        private readonly ICarRepository _repository;
+
+        public GetCarCountQueryHandler(ICarRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetCarCountQueryResult> Handle(GetCarCountQuery request, CancellationToken cancellationToken)
+        {
+            var value = _repository.GetCarCount();
+            return new GetCarCountQueryResult{
+                CarCount = value
+            };
+
+        }
+    }
+}
---
+++ CarBookApplication/Features/Mediator/Queries/StatisticsQueries/GetCarCountQuery.cs
@@ -0,0 +1,9 @@
+﻿using CarBookApplication.Features.Mediator.Results.StatisticsResults;
+using MediatR;
+
+namespace CarBookApplication.Features.Mediator.Queries.StatisticsQueries
+{
+    public class GetCarCountQuery : IRequest<GetCarCountQueryResult>
+    {
+    }
+}
