7fbc6426f737add310ee9c3a065a449592fc8d79
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-08-29 17:39:51 +0300

    reservation-location entites multiple relation created

---
+++ CarBookWebUI/Controllers/ReservationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBookWebUI.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Rent A Car";
+            ViewBag.v2 = "Rent A Car Form";
+            return View();
+        }
+    }
+}
