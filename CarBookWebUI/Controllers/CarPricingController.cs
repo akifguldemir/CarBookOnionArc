adec84693317863c97ab4f6f84d029b03e4c9dbb
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-02 14:32:43 +0300

    Cars with time period price listed in frontend

--- CarBookWebUI/Controllers/CarPricingController.cs
+++ CarBookWebUI/Controllers/CarPricingController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBookDto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace CarBookWebUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        public IActionResult Index()
-        {
-            ViewBag.v1 = "Cars";
-            ViewBag.v2 = "Prices";
-            return View();
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CarPricingController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IActionResult> Index()
+		{
+			ViewBag.v1 = "Cars";
+			ViewBag.v2 = "Prices";
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7065/api/CarPricings/GetCarPricingWithTimePeriodList");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
