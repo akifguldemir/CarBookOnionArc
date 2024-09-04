a6f2acd27388a425b9fe46ba13def360e61b1990
AKİF GÜLDEMİR <akif.guldemir@ozyer.com>
2024-09-04 10:59:39 +0300

    AppUser and AppRole created

---
+++ CarBookPersistence/Migrations/20240904075416_app_user.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBookPersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class app_user : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarFeatures_Features_FeatureID",
+                table: "CarFeatures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FeatureID",
+                table: "CarFeatures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AppRoles",
+                columns: table => new
+                {
+                    AppRoleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppRoleName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppRoles", x => x.AppRoleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    AppUserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppRoleId = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.AppUserId);
+                    table.ForeignKey(
+                        name: "FK_AppUsers_AppRoles_AppRoleId",
+                        column: x => x.AppRoleId,
+                        principalTable: "AppRoles",
+                        principalColumn: "AppRoleId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUsers_AppRoleId",
+                table: "AppUsers",
+                column: "AppRoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarFeatures_Features_FeatureID",
+                table: "CarFeatures",
+                column: "FeatureID",
+                principalTable: "Features",
+                principalColumn: "FeatureID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarFeatures_Features_FeatureID",
+                table: "CarFeatures");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+
+            migrationBuilder.DropTable(
+                name: "AppRoles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FeatureID",
+                table: "CarFeatures",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarFeatures_Features_FeatureID",
+                table: "CarFeatures",
+                column: "FeatureID",
+                principalTable: "Features",
+                principalColumn: "FeatureID");
+        }
+    }
+}
---
+++ CarbookDomain/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbookDomain.Entities
+{
+	public class AppUser
+	{
+        public int AppUserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int AppRoleId { get; set; }
+        public string Email { get; set; }
+        public AppRole AppRole { get; set; }
+    }
+}
